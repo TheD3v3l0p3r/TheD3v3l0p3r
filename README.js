1f8177912dc57890379b64e204fd54d50b7603f1
TheD3v3l0p3r <166638842+TheD3v3l0p3r@users.noreply.github.com>
2024-07-09 13:55:05 -0700

    About Me

---
+++ README.js
@@ -0,0 +1,29 @@
+/*
+Hey there!
+Welcome to my README page.
+I am... what you would call a "Noob".
+I'm currently learning Python and Javascript (JS).
+I want to pursue a career in Cybersecurity, Data Science, and (possibly) Web Development.
+I'm not looking for Collabs just yet; I might later on.
+You can reach me at Telegram: , Facebook: , LinkedIn: , X (formerly, Twitter): ; Discord: .
+
+
+"Learning ain't easy; doesn't have to be difficult either." That's my mono.
+*/
+
+
+
+
+
+- 👋 Hi, I’m @TheD3v3l0p3r
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+
+<!---
+TheD3v3l0p3r/TheD3v3l0p3r is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
